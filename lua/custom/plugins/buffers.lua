9f5640d05445e5b9ac16332043e608280fd1c731
Jude Martin <j.martin24@imperial.ac.uk>
2026-08-30 19:16:10 +0100

    added <leader bf>

--- lua/custom/plugins/buffers.lua
+++ lua/custom/plugins/buffers.lua
@@ -6,3 +6,4 @@ require('mini.bufremove').setup {}
 vim.keymap.set('n', '<leader>bn', '<cmd>BufferLineCycleNext<CR>', { desc = '[B]uffer [N]ext' })
 vim.keymap.set('n', '<leader>bp', '<cmd>BufferLineCyclePrev<CR>', { desc = '[B]uffer [P]rev' })
 vim.keymap.set('n', '<leader>bk', function() require('mini.bufremove').delete(0, false) end, { desc = '[B]uffer [K]ill' })
+vim.keymap.set('n', '<leader>bf', function() require('telescope.builtin').buffers({ initial_mode = 'insert' }) end, { desc = '[B]uffer [F]ind' })
